914584c125cf1c6cb35b950d4844642cc7474eed
deivi <trianadeivi92@gmail.com>
2025-09-06 16:24:40 -0500

    Corregir importación de useAuth en ProtectedRoute

--- frontend/src/auth/ProtectedRoute.tsx
+++ frontend/src/auth/ProtectedRoute.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from './useAuth';
+import { useAuth } from './hooks/useAuth';
 import { Box, CircularProgress } from '@mui/material';
 
 /**
